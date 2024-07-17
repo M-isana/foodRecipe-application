adbaddb776b260e4adce8e18ef46151e314c3fdd
Manasi Sasane <manasisasane1999@gmail.com>
2024-07-17 15:55:53 +0530

    added components

--- food-recipe-app/src/App.js
+++ food-recipe-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!!!</h1>
+      <h1>Hello World!!!</h1>
+      <Home />
     </div>
   );
 }
---
+++ food-recipe-app/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router'
+
+const NavBar = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default NavBar
